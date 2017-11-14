ade25d0a32bea8e590620435194cbd68029767f4
Moercell <marcel.pahnke@gmail.com>
2017-11-14 12:26:02 +0100

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { ItemsComponent } from './components/items/items.component';
+
+import { ItemService } from './services/item.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'firebase01'),
+    AngularFirestoreModule,
+    FormsModule
+  ],
+  providers: [ItemService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+    firebase: {
+    apiKey: "AIzaSyC6vpbQ5eEa15SXHrEiAzAwF6af4lnaMDQ",
+    authDomain: "ng-test01-e3379.firebaseapp.com",
+    databaseURL: "https://ng-test01-e3379.firebaseio.com",
+    projectId: "ng-test01-e3379",
+    storageBucket: "ng-test01-e3379.appspot.com",
+    messagingSenderId: "474877870055"
+
+  }
+};
